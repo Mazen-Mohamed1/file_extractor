5381703f2925e76c890b62a24e4c5a9eca190946
Mazen Mohamed <114352651+Mazen111231@users.noreply.github.com>
2023-11-29 00:15:21 +0200

    Add files via upload

---
+++ extract.py
@@ -0,0 +1,37 @@
+import PySimpleGUI as sg
+from zipfile import ZipFile
+
+# user zip file
+
+
+
+#  the Gui
+sg.theme("DarkSlateBlue4")
+
+layout = [
+    [sg.Text("FILE PATH", size=(15,1)), sg.InputText(key="FILE PATH")],
+    [sg.Button('Extract'),sg.Exit()]
+]
+
+window = sg.Window('Mazen for Extracting files',layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Exit":
+        break
+    elif event == "Extract":
+        fileName = values["FILE PATH"].strip('"')
+
+        with ZipFile(fileName, "r") as Zip:
+            extracted_files = Zip.namelist()
+            sg.popup("Files extracted:\n" + "\n".join(extracted_files))
+
+            Zip.extractall()
+        sg.popup("Extraction completed")
+    else:
+        sg.popup("please enter the file path")
+
+
+
+
+window.close()
